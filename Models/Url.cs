54ca83a17c4cb9ff668edc1dd1ab31b9ef50cc3c
Stuart McCann <smccann74@qub.ac.uk>
2022-06-29 13:21:27 +0100

    Refactor Url.cs

--- Models/Url.cs
+++ Models/Url.cs
@@ -5,41 +5,9 @@ namespace UrlShorterner.Models
 {
     public class Url
     {
-
-        public static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
-        public static readonly int Base = Alphabet.Length;
-
         public int UrlID { get; set; }
         public string OriginalUrl { get; set; }
         public string ShortUrl { get; set; }
         public DateTime CreatedDate { get; set; }
-
-
-        public  string Encode(int i)
-        {
-            //if (i == 0) return Alphabet[0].ToString();
-
-            var s = string.Empty;
-
-            while (i > 0)
-            {
-                s += Alphabet[i % Base];
-                i = i / Base;
-            }
-
-            return string.Join(string.Empty, s.Reverse());
-        }
-
-        public  int Decode(string s)
-        {
-            var i = 0;
-
-            foreach (var c in s)
-            {
-                i = (i * Base) + Alphabet.IndexOf(c);
-            }
-
-            return i;
-        }
     }
 }
